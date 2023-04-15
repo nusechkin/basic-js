e08baff3142c5e81baa185a041fd274b49f81198
nusechkin <komarova.ao@gmail.com>
2023-04-15 14:45:15 +0300

    Feat: dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) { //проверка на входящий тип
+    return false;
+  }
+  const allStringArr = members.filter(word => typeof word === 'string'); //убрали всё, что не строка
+  const nospacesArr = allStringArr.map(word => word.replace(/\s/g, '')); //убрали лишние пробелы
+  const name = nospacesArr.map(word => word.toUpperCase().charAt(0)).sort(); //сначала забрали первую букву, потом отсортировали
+  return name.join(''); //склеили
 }
 
 module.exports = {
