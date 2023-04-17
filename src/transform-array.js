1b522ea68b68bc23a83a84b6bc705eb451bc3380
nusechkin <komarova.ao@gmail.com>
2023-04-17 15:17:59 +0300

    Feat: transform-array 1/2

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  if (arr === []) {
+    return arr;
+  }
+  let transfrormArr = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+  var clone = arr.slice(0);
+  let found = clone.findIndex(elem => elem === '--discard-next');
+  if (typeof found === Number) {
+    clone.splice(found, 2);
+  }
+  found = clone.findIndex(elem => elem === '--discard-prev');
+  if (typeof found === Number) {
+    clone.splice(found, 1);
+    if (found > 0) {
+      //clone.splice(found - 1, 2);
+    }
+    else {
+      //clone.splice(found, 2);
+    }
+  }
+  // found = clone.findIndex(elem => elem === '--double-next');
+  // if (found) {
+  //   clone.splice(found, 1);
+  //   clone.splice(found, 0, clone[found]);
+  // }
+  return clone;
 }
 
 module.exports = {
